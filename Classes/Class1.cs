50dcc2fefa2fc9be42e7751e7d58e36451e1065d
russelibale808 <russelibale808@gmail.com>
2022-10-28 05:09:14 -1000

    Classes Exercise

---
+++ Classes/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Classes
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+
+        public Car()
+        {
+
+        }
+        public Car(string brand, string model, string year)
+        {
+            Name = brand;
+            Model = model;
+            Year = year;
+        }
+
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,9 +2,20 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myFirstCar = new Car();
+
+
+            myFirstCar.Name = "Nissan";
+            myFirstCar.Model = "Sentra SR";
+            myFirstCar.Year = "2014";
+
+            Console.WriteLine($" My first car is a {myFirstCar.Name}." +
+                              $" I got the {myFirstCar.Model} model," +
+                              $" which is {myFirstCar.Year} version." );
+
         }
     }
 }
